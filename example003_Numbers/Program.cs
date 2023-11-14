5883620ef7fa4cf09e9bd40156cfc1e556c86875
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-11-14 22:45:40 +0300

    new Random().Next(min,max) added

--- example003_Numbers/Program.cs
+++ example003_Numbers/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // 1,2,3, .. max = 9
+Console.WriteLine(numberA); // Выводит число в переменной numberA
+int numberB = new Random().Next(1,10);// 1,2,3, .. max = 9
+Console.WriteLine(numberB); //Выводит число в переменной numberB
 Console.WriteLine(numberA + numberB); 
