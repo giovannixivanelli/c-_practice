03fa9fcd5399f5518a9f16ad3449db1a20f130e9
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-11-14 22:54:46 +0300

    IfElse added

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Your Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Wow, it's Masha!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
