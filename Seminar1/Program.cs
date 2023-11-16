19122f9f6b3d66691c792c00c5c804497ffd9369
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-11-16 21:03:23 +0300

    Seminar1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int first = 12;
+int second = new Random().Next(1,100);
+if (first > second)
+{
+    Console.WriteLine($"{first} greater than {second}");
+}
+else 
+{
+    Console.WriteLine($"{second} greater than {first}");
+}
