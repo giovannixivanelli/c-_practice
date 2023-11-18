e35c2bac8790141edaf16c9c576a0d1e98cf09c3
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-11-18 23:58:12 +0300

    Method with array example

---
+++ example010_MethoArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {21,24,53,14,81, 25,36,57,81};
+
+int n = array.Length; // присваем n значение количества индексов в массиве
+int find = 81;
+
+int index = 0; // счетчик, чтоб можно было перебрать все индексы в массиве, начиная с 0
+
+while (index < n)
+    {
+        if(array[index] == find)
+            {
+                Console.WriteLine(index);
+                break; // прерывает цикл
+            }
+        index ++;
+
+    }
