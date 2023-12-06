0582ae0094a90492321a3cd76fbdd1f737276ce5
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-12-07 01:59:38 +0300

    seminar5 hw

--- Seminar5_Homework/Program.cs
+++ Seminar5_Homework/Program.cs
@@ -5,55 +5,55 @@
 
 */
 
-// int [,] Create2dArray(int row, int col, int min, int max){
-//     int [,] array = new int [row,col];
-//     for (int i =0; i<row; i++){
-//         for(int j = 0; j<col;j++){
-//             array[i,j] = new Random().Next(min, max+1);
-//         }
-//     }
-//     return array;
-// }
+int [,] Create2dArray(int row, int col, int min, int max){
+    int [,] array = new int [row,col];
+    for (int i =0; i<row; i++){
+        for(int j = 0; j<col;j++){
+            array[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
 
-// void Show2dArray(int [,] array){
-//     for(int i = 0; i<array.GetLength(0); i++){
-//         for(int j=0; j<array.GetLength(1); j++){
-//             Console.Write(array[i,j]+" ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void Show2dArray(int [,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j=0; j<array.GetLength(1); j++){
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
-// Console.WriteLine("Введите количество строк: ");
-// int row = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите количество столбиков: ");
-// int col = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите минимальное значение элемента массива: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите максимальное значение элемента массива: ");
-// int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбиков: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-// int [,] created2dArray = Create2dArray(row,col,min,max);
+int [,] created2dArray = Create2dArray(row,col,min,max);
 
-// Console.WriteLine("Получился такой массив: ");
-// Show2dArray(created2dArray);
-//  Console.WriteLine();
+Console.WriteLine("Получился такой массив: ");
+Show2dArray(created2dArray);
+ Console.WriteLine();
 
-// Console.WriteLine("Укажите номер строки элемента: ");
-// int findRow = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Укажите номер столбика элемента: ");
-// int findCol= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите номер строки элемента: ");
+int findRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите номер столбика элемента: ");
+int findCol= Convert.ToInt32(Console.ReadLine());
 
-// if (findRow >= 0 && findRow < created2dArray.GetLength(0) && findCol >= 0 && findCol < created2dArray.GetLength(1))
-//     {
-//             int element = created2dArray[findRow, findCol]; 
-//             Console.WriteLine($"Значение элемента массива: {element}");
-//     }
-// else
-//     {
-//             Console.WriteLine("Указанный элемент массива не существует.");
-//     }
+if (findRow >= 0 && findRow < created2dArray.GetLength(0) && findCol >= 0 && findCol < created2dArray.GetLength(1))
+    {
+            int element = created2dArray[findRow, findCol]; 
+            Console.WriteLine($"Значение элемента массива: {element}");
+    }
+else
+    {
+            Console.WriteLine("Указанный элемент массива не существует.");
+    }
 
 /*
 
@@ -62,56 +62,122 @@
 
 */
 
-// int [,] Create2dArray(int row, int col, int min, int max){
-//     int [,] array = new int [row,col];
-//     for (int i =0; i<row; i++){
-//         for(int j = 0; j<col;j++){
-//             array[i,j] = new Random().Next(min, max+1);
-//         }
-//     }
-//     return array;
-// }
+int [,] Create2dArray(int row, int col, int min, int max){
+    int [,] array = new int [row,col];
+    for (int i =0; i<row; i++){
+        for(int j = 0; j<col;j++){
+            array[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
 
-// void Show2dArray(int [,] array){
-//     for(int i = 0; i<array.GetLength(0); i++){
-//         for(int j=0; j<array.GetLength(1); j++){
-//             Console.Write(array[i,j]+" ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
+void Show2dArray(int [,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j=0; j<array.GetLength(1); j++){
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
-// Console.WriteLine("Введите количество строк: ");
-// int row = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите количество столбиков: ");
-// int col = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите минимальное значение элемента массива: ");
-// int min = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите максимальное значение элемента массива: ");
-// int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбиков: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
 
-// int [,] created2dArray = Create2dArray(row,col,min,max);
+int [,] created2dArray = Create2dArray(row,col,min,max);
 
-// Console.WriteLine("Получился такой массив: ");
-// Show2dArray(created2dArray);
-// Console.WriteLine();
+Console.WriteLine("Получился такой массив: ");
+Show2dArray(created2dArray);
+Console.WriteLine();
 
-// int newRow = created2dArray.GetLength(0);
-// int newCol = created2dArray.GetLength(1);
+int newRow = created2dArray.GetLength(0);
+int newCol = created2dArray.GetLength(1);
 
-//         for (int j = 0; j < newCol; j++)
-//         {
-//             int temp = created2dArray[0, j];
-//             created2dArray[0, j] = created2dArray[newRow - 1, j];
-//             created2dArray[newRow - 1, j] = temp;
-//         }
-// Console.WriteLine("Массив после замены местами первой и последней строки:");
-//         for (int i = 0; i < created2dArray.GetLength(0); i++)
-//         {
-//             for (int j = 0; j < created2dArray.GetLength(1); j++)
-//             {
-//                 Console.Write(created2dArray[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
+        for (int j = 0; j < newCol; j++)
+        {
+            int temp = created2dArray[0, j];
+            created2dArray[0, j] = created2dArray[newRow - 1, j];
+            created2dArray[newRow - 1, j] = temp;
+        }
+Console.WriteLine("Массив после замены местами первой и последней строки:");
+        for (int i = 0; i < created2dArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < created2dArray.GetLength(1); j++)
+            {
+                Console.Write(created2dArray[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+/*
+
+Задача 3: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+*/
+
+int [,] Create2dArray(int row, int col, int min, int max){
+    int [,] array = new int [row,col];
+    for (int i =0; i<row; i++){
+        for(int j = 0; j<col;j++){
+            array[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int [,] array){
+    for(int i = 0; i<array.GetLength(0); i++){
+        for(int j=0; j<array.GetLength(1); j++){
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.WriteLine("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбиков: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [,] created2dArray = Create2dArray(row,col,min,max);
+
+Console.WriteLine("Получился такой массив: ");
+Show2dArray(created2dArray);
+Console.WriteLine();
+
+int MinRowIndex(int[,] array)
+{
+    int min = int.MaxValue;
+    int index = 0;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int minIndex = MinRowIndex(created2dArray);
+Console.WriteLine($"Индекс строки с наименьшей суммой элементов: {minIndex}");
